0cdb0c65263c26abffeb37b99c2bfb781f590dfb
Chelsea Wagner <1chelsea.fischer1@gmail.com>
2023-11-05 21:03:39 -0800

    updated contact page with a form still need to add email validator and messages if you click off the page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,36 @@
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
 export default function Contact() {
     return (
         <div>
+          <Form.Label htmlFor="basic-url">Name</Form.Label>
+            <InputGroup className="mb-3">
+            <Form.Control
+                placeholder="Name"
+                aria-label="Name"
+                aria-describedby="basic-addon1"
+             />
+            </InputGroup>
+
+         <Form.Label htmlFor="basic-url">Email</Form.Label>
+            <InputGroup className="mb-3">
+            <Form.Control
+                placeholder="Email Address"
+                aria-label="Email-Address"
+                aria-describedby="basic-addon2"
+            />
+            </InputGroup>
+
+        <Form.Label htmlFor="basic-url">Message</Form.Label>
+            <InputGroup className="mb-3">
+            <Form.Control 
+                aria-label="message"
+                 placeholder="Message"
+                 aria-describedby="basic-addon3"
+                 />
+            </InputGroup>
+       <Button type="submit">Submit form</Button>
         </div>
     )
 }
