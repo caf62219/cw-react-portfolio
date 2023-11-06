7a85c42726dde96e16d7e45af30fd02d37a8c700
Chelsea Wagner <1chelsea.fischer1@gmail.com>
2023-11-05 21:38:51 -0800

    added in resume document

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,28 @@
-export default function Resume() {
+ import React from 'react';
+ 
+ export default function Resume() {
     return (
         <div>
+            <div>
+                <h3>Proficiencies</h3>
+                <h4> Software/Programming Languages </h4>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>MySQL</li>
+                    <li>MongoDB</li>
+                    <li>node.js</li>
+                    <li>express.js</li>
+                    <li>React</li>
+                    <li>GitHub</li>
+                    <li>Heroku</li>
+                    <li>Jest</li>
+                </ul>
+                <h4>Soft Skills</h4>
+            </div>
+            <iframe src=”path_to_pdf_file.pdf” width=”100%” height=”500px” />
+            <button type="button" class="btn btn-primary">Download Resume</button>
         </div>
     )
 }
